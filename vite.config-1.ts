f12ca3f6c28e6dadd087ad199b6f5dd67e9c4bde
Travis Giffin <travisgiffin1122@gmail.com>
2025-03-20 11:20:20 -0600

    build update file
    I changed these so they could build the JS file properly

--- vite.config-1.ts
+++ vite.config-1.ts
@@ -1,3 +1,4 @@
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -11,7 +12,8 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' && componentTagger(),
+    mode === 'development' &&
+    componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
@@ -39,9 +41,5 @@ export default defineConfig(({ mode }) => ({
         inlineDynamicImports: true,
       }
     }
-  },
-  // ✅ Fix: Ensure `process.env.NODE_ENV` is defined during build
-  define: {
-    'process.env.NODE_ENV': JSON.stringify(mode),
   },
 }));
